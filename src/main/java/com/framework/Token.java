88ea5539fe51252d193c88190c3fe554cf454c28
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-02-11 19:10:32 +0200

    This is the representation of the Token that will be parsed into the parser soon

---
+++ src/main/java/com/framework/Token.java
@@ -0,0 +1,26 @@
+package com.framework;
+
+/**
+ * Implementation of the Token class
+ * 
+ * @author Hlulani Myambo
+ * @version 10 Feb 2026
+ */
+public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    public Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    @Override
+    public String toString() {
+        return this.type + " " + this.lexeme + " " + literal;
+    }
+}
