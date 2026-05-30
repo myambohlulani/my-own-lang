f1f0906f7e139c82f4e2f2e23ec6a80100a3fba0
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-05-30 03:24:08 +0200

    feat (lexer): trying to read everything line by line but manually

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "./lexer.hpp"
+#include <fstream>
+#include <string>
 
 int main(void) {
-    std::cout << "Dear compiler" << sdt::endl;
+    // manual way of doing the lexing
+    std::ifstream file("main.hlx");
+
+    if(!file.is_open()) {
+        std::cout << "Failed to open the file\n";
+        return 1;
+    }
+
+    std::string line;
+
+    while(std::getline(file, line)) {
+        std::cout << line << "\n";
+    }
 }
