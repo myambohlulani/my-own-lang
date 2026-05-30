b69a926b724bd3c3412a4381800e56f51ec349b7
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-05-30 20:57:35 +0200

    feat (main): basic main cli arguments

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	std::cout << argc << std::endl;
+	std::cout << argv[0] << std::endl;
+
+	std::cout << "Hello, World" << std::endl;
+	return 0;
+}
+
+
