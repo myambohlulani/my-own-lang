d21da158e617bbbe9f74dcbe6183171a17225533
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-05-30 22:49:34 +0200

    feat (main): added the way of reading contents from a certain file - does not work yet and a way to validate the extension of a given file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+#include "./main.h"
+
 
 int main(int argc, char* argv[]) {
-	std::cout << argc << std::endl;
-	std::cout << argv[0] << std::endl;
+	if(argc != 2 ) {
+		// we have more than 2 or less than 2 arguments
+		std::cerr << "Incorrect usage of the compiler." << std::endl;
+		std::cerr << "The correct usage is: radium <filename.hlx> or <filename.hl>" << std::endl;
 
-	std::cout << "Hello, World" << std::endl;
-	return 0;
+		return EXIT_FAILURE;
+	}
+
+	// checking for correct extension
+	std::string extension;
+	std::string filename = argv[1];
+
+	if(correct_extension(filename, extension) == EXIT_SUCCESS){
+		std::stringstream contents_stream;
+		std::string contents;
+
+
+		/* reading a file
+	 	* argv[0] is the path and std::ios::in is for reading only.
+		 * It will automatically close the buffer due to the scope.
+		 */
+		{
+	       		std::fstream input(argv[0], std::ios::in);
+			// reading the whole thing from the file
+			contents_stream << input.rdbuf();
+			contents = contents_stream.str();
+		}
+
+		std::cout << contents << std::endl;
+	}
+
+	std::cout << extension << std::endl;
+
+	return EXIT_SUCCESS;
 }
-
-
