73a5f6d53ce0afa8f3e251b3cbdfd68e83101c8a
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-05-30 03:23:26 +0200

    feat (lexer.hpp): added the token names

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -1,4 +1,23 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+enum Token {
+    // control flow
+    IF_KEY,
+    ELSEIF_KEY,
+    ELSE_KEY,
+    WHILE_KEY,
+    FOR_KEY,
+    // data type
+    INT_KEY,
+
+    // operators
+    EQUALS_OP,
+    PLUS_OP,
+    DIVIDE_OP,
+    MINUS_OP,
+
+    SEMICOLON
+};
+
 #endif // LEXER_H
