9d82d9f1248c401863f78913e8b051a66774a48c
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-07-03 12:51:36 +0200

    fix (parser): change offset from 0 to 1 since i was giving outof bounds, corrected the member attribute, consuming the characters the way it is supposed to and adding an error and correct token check, removed the namespace and leave the structs for the expressions outside

--- src/parser.hpp
+++ src/parser.hpp
@@ -4,18 +4,15 @@
 #include "./lexer.hpp"
 #include <optional>
 
-// These nodes are for expressions and doing calculations
-namespace ParserNode {	
-	// 0 + 2 , 2 - 3 ..etc
-	struct NodeExpr{
-		Token int_lit;
-	};
+// These nodes are for expressions and doing calculations	
+// 0 + 2 , 2 - 3 ..etc
+struct NodeExpr{
+	Token int_lit;
+};
 
-	// exit(0) ..etc
-	struct NodeExit {
-		NodeExpr exit;
-	};
-
+// exit(0) ..etc
+struct NodeExit {
+	NodeExpr expr;
 };
 
 class Parser {
@@ -26,24 +23,27 @@ class Parser {
 		inline explicit Parser(std::vector<Token> tokens) : m_tokens(std::move(tokens)) {
 		}
 
-		inline std::optional<ParserNode::NodeExit> parse() {
-			std::optional<ParserNode::NodeExit> exit_node;
+		inline std::optional<NodeExit> parse() {
+			std::optional<NodeExit> exit_node;
 
 			while(peek().has_value()) {
-				if(peek().value().type = TokenType::EXIT) {
+				if(peek().value().type == TokenType::EXIT) {
 					consume(); // consuming exit
 					if(auto node_expr = parse_expr()) {
-						exit_node = ParserNode::NodeExit(.expr = node_expr.value())
+						exit_node = NodeExit{.expr = node_expr.value()};
 					} else {
 						std::cerr << "Invalid expression" << std::endl;
 						exit(EXIT_FAILURE);
 					}
 
 					// semi colon
-					if(!peek().has_value() || peek().value().type != TokenType::SEMICOLON) {
+					if(peek().has_value() && peek().value().type == TokenType::SEMICOLON) {
+						consume();
+					} else {
 						std::cerr << "There is no semicolon" << std::endl;
 						exit(EXIT_FAILURE);
 					} 
+
 				}
 			}
 	
@@ -54,20 +54,20 @@ class Parser {
 		/**
 		This method parsers and expression
 		*/
-		inline std::optional<ParserNode::NodeExpr> parse_expr() {
+		inline std::optional<NodeExpr> parse_expr() {
 			if(peek().has_value() && peek().value().type == TokenType::INT_LIT) {
-				return ParserNode::NodeExpr{.INT_LIT = consume()}; // consuming the literal
+				return NodeExpr{.int_lit = consume()}; // consuming the literal
 			} else {
 				return {};
 			}	
 		}
 
 		// This method look for the next token with an offset of 0, can be incremented by an offset
-		[[nodiscard]] inline std::optional<Token> peek(int offset = 0) const {
+		[[nodiscard]] inline std::optional<Token> peek(int offset = 1) const {
 			if(m_index + offset > m_tokens.size()) {
 				return {};
 			} else {
-				return m_tokens.at(m_index + offset );
+				return m_tokens.at(m_index);
 			}
 		}
 
