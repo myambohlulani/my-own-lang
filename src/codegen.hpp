215fcb0f75df73c49a431cf587c72911751d5051
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-07-10 12:56:46 +0200

    fix (codegen): removed the const and allowed more variables creations, add method that generates all the statements instead of one by one

--- src/codegen.hpp
+++ src/codegen.hpp
@@ -4,24 +4,37 @@
 
 class Generator {
 public:
-  inline explicit Generator(NodeExit root) : m_root(std::move(root)) {}
+  inline explicit Generator(NodeProgram root) : m_root(std::move(root)) {}
 
   [[nodiscard]] inline std::string generate() const {
+    std::stringstream output;
+    output << m_start;
+
+    for (const NodeStatement& stmt : m_root.statements) {
+      // TODO: generate all the statements
+    }
+
+    return output.str();
+  }
+
+  [[nodiscard]] inline std::string generate_exit() const {
     std::stringstream output;
     output << m_start; // start part
 
     // login the code for the exit
     output << "   li $v0, 4001\n";
-    output << "   li $a0, " << m_root.expr.int_lit.value.value() << "\n";
-    output << "   " << m_syscall;
+    // output << "   li $a0, " << m_root.expr.int_lit.value.value() << "\n";
+    output << "   " << m_syscall << "\n";
 
     return output.str();
   }
 
 private:
-  const NodeExit m_root{};
+  const NodeProgram m_root{};
   const std::string m_start = ".text\n.globl __start\n__start:\n";
   const std::string m_syscall = "syscall\n";
+  int  m_label_count = 0;
+  int  m_var_count = 0;
 };
 
 #endif // CODEGEN_H
