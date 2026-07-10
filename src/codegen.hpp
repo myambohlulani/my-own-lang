6fe663fff8201843a54d862da67f672831dfc0ec
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-07-10 14:44:53 +0200

    fix (codegen): allowing only one exit to be used

--- src/codegen.hpp
+++ src/codegen.hpp
@@ -10,13 +10,21 @@ public:
     std::stringstream output;
     output << m_start;
 
-    for (const NodeStatement& stmt : m_root.statements) {
-      //TODO: Generate all the statements
+    // allowing only 1 exit, the first one
+    bool has_exit = false;
+
+    for (const NodeStatement &stmt : m_root.statements) {
+      if (std::holds_alternative<NodeExit>(stmt.var)) {
+        has_exit = true;
+      }
+
       output << generate_statements(stmt);
     }
 
-    // TODO: add a generate default_exit method,
-    output << generate_default_exit();
+    if (!has_exit) {
+      output << generate_default_exit();
+    }
+
     return output.str();
   }
 
@@ -24,33 +32,35 @@ private:
   const NodeProgram m_root{};
   const std::string m_start = ".text\n.globl __start\n__start:\n";
   const std::string m_syscall = "syscall\n";
-  int  m_label_count = 0;
-  int  m_var_count = 0;
+  int m_label_count = 0;
+  int m_var_count = 0;
 
-  [[nodiscard]] inline std::string generate_exit(const NodeExit& node) const {
+  [[nodiscard]] inline std::string generate_exit(const NodeExit &node) const {
     std::stringstream output;
 
     // login the code for the exit
     output << "   li $v0, 4001\n";
-    output << "   li $a0, " << node.expr.int_lit.value.value() << "\n";
+    output << "   li $a0, 0x01\n";
+    output << "   li $a1, " << node.expr.int_lit.value.value() << "\n";
     output << "   " << m_syscall << "\n";
 
     return output.str();
   }
 
-  [[nodiscard]] inline std::string generate_statements(const NodeStatement& stmt) const {
+  [[nodiscard]] inline std::string
+  generate_statements(const NodeStatement &stmt) const {
     std::stringstream output;
 
     struct visit_statement {
-      const Generator* gen;
-      std::stringstream& output;
+      const Generator *gen;
+      std::stringstream &output;
 
-      void operator()(const NodeExit& node) const {
-        output << gen -> generate_exit(node);
+      void operator()(const NodeExit &node) const {
+        output << gen->generate_exit(node);
       }
 
-      void operator()(const NodePrintf& node) const {
-        output << gen -> generate_print_int_stmt(node);
+      void operator()(const NodePrintf &node) const {
+        output << gen->generate_print_int_stmt(node);
       }
 
       // TODO: Create if statements and more
@@ -71,16 +81,17 @@ private:
     return output.str();
   }
 
-  [[nodiscard]] inline std::string generate_print_int_stmt(const NodePrintf& node) const {
+  [[nodiscard]] inline std::string
+  generate_print_int_stmt(const NodePrintf &node) const {
     std::stringstream output;
     output << "   li $v0, 4004\n";
     output << "   li $a0, 1\n"; // file descriptor stdout
-    output << "   li $a1, " << node.expr.int_lit.value.value() << "\n" ; // TODO: print the integer only
+    output << "   li $a1, " << node.expr.int_lit.value.value()
+           << "\n";             // TODO: print the integer only
     output << "   li $a2, 8\n"; // byte for the number
     output << "   " << m_syscall << "\n";
     return output.str();
   }
-
 };
 
 #endif // CODEGEN_H
