24cec7484e927cd5ca8aef09b4f8608997fe7df8
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-07-18 23:15:55 +0200

    fix(main): remove the 4 args part, changed the checking of empty file from using just double quotes inito .empty method

--- src/main.cpp
+++ src/main.cpp
@@ -7,70 +7,19 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  if (argc == 4) {
-    // has radium -> file -> -o -> what is required to output
-    if ("-o" != argv[2]) {
-      std::cerr << "The -o must come after the file you want then assembly file" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    // checking for correct extension
-    std::string extension;
-    const std::string filename = argv[1];
-    // const std::string filename = "../hello.hlx"; // for debuging
-
-    // checking for correct extension
-    if (correct_extension(filename, extension) == EXIT_SUCCESS) {
-      std::string contents = read_file_contents(filename); // reading the file
-
-      // did not read anything so it is considered a failure.
-      if ((contents == "")) {
-        return EXIT_FAILURE;
-      }
-
-      // start lexing or converting into tokens
-      Lexer lex(std::move(contents));
-      Lexer *lexer = &lex;
-
-      const std::vector<Token> tokens = lexer->tokenize(); // tokens
-
-      // Parser
-      Parser par(tokens);
-      Parser *parser = &par;
-      const std::optional<NodeProgram> tree = parser -> parse();
-
-      if (!tree.has_value()) {
-        std::cerr << "The program is invalid haha:-)" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      // generator
-      const Generator gen(tree.value()); // TODO: Change this
-      const Generator *generator = &gen;
-
-      // generating the code
-      const std::string strs = generator-> generate();
-      if (strs.empty()) {
-        exit(EXIT_FAILURE);
-      }
-
-      write_contents_into_a_file(strs, argv[3], argv[1]);
-    }
-  }
-  // default one
-  else if (argc == 2) {
+  if (argc == 2) {
     // has radium and the file
     // checking for correct extension
     std::string extension;
     const std::string filename = argv[1];
-    // const std::string filename = "../hello.hlx"; // for debuging
+    // const std::string filename = "../hello.hlx";
 
     // checking for correct extension
     if (correct_extension(filename, extension) == EXIT_SUCCESS) {
       std::string contents = read_file_contents(filename); // reading the file
 
       // did not read anything so it is considered a failure.
-      if ((contents == "")) {
+      if ((contents.empty())) {
         return EXIT_FAILURE;
       }
 
